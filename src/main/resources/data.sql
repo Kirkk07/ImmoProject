97b29bcb0856f181bd8191d0b012f2c795b2a3dd
sabire <hasankorpe07@gmail.com>
2025-09-28 13:28:48 +0200

    Versie 4 - 28/09 p34 6.5 exercise

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
 insert into venue (venue_name, link_more_info,capacity,food_provided,indoor,outdoor,free_parking_available,city,distance_from_public_transport_in_km,image_url) values
 ('De Club', 'https://transitm.mechelen.be/de-club',150,false, true,false,
- true,'Mechelen',2,'https://transitm.mechelen.be/de-club');
+ true,'Mechelen',2,'/img/venue_club.jpg');
