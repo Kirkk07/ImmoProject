7f47115227bd8ed4d6d10a8af5f1eacc190f8a32
sabire <hasankorpe07@gmail.com>
2025-10-06 22:22:37 +0200

    Versie 6.2 - p26

---
+++ src/main/java/be/thomasmore/party/controllers/ArtistController.java
@@ -0,0 +1,27 @@
+package be.thomasmore.party.controllers;
+
+import be.thomasmore.party.model.Artist;
+import be.thomasmore.party.model.Venue;
+import be.thomasmore.party.repositories.ArtistRepository;
+import be.thomasmore.party.repositories.ClientRepository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
+
+public class ArtistController {
+    private ArtistRepository artistRepository;
+
+    public ArtistController(ArtistRepository artistRepository) {
+        this.artistRepository = artistRepository;
+    }
+    @GetMapping("/artistlist")
+    public String artistList(Model model) {
+        Iterable<Artist> artists = artistRepository.findAll();
+        model.addAttribute("artists", artists);
+        return "artistlist";
+    }
+
+
+}
---
+++ src/main/java/be/thomasmore/party/model/Artist.java
@@ -0,0 +1,78 @@
+package be.thomasmore.party.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Artist {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+   public String artistName;
+    @Column(length = 1000)
+    private String bio;
+
+   public String genre;
+   public String linkMoreInfo;
+    public String portfolio;
+    public String imageUrl;
+
+    public String getPortfolio() {
+        return portfolio;
+    }
+
+    public void setPortfolio(String portfolio) {
+        this.portfolio = portfolio;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getLinkMoreInfo() {
+        return linkMoreInfo;
+    }
+
+    public void setLinkMoreInfo(String linkMoreInfo) {
+        this.linkMoreInfo = linkMoreInfo;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+
+
+}
