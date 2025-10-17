c71c2e5eb005682fd20de80b983efd4f5d6b6485
Hasan Korpe <hasankorpe07@gmail.com>
2025-10-17 12:38:23 +0200

    page 17 4.8 Search Artist list

--- src/main/java/be/thomasmore/party/controllers/ArtistController.java
+++ src/main/java/be/thomasmore/party/controllers/ArtistController.java
@@ -4,10 +4,13 @@ import be.thomasmore.party.model.Artist;
 import be.thomasmore.party.model.Venue;
 import be.thomasmore.party.repositories.ArtistRepository;
 import be.thomasmore.party.repositories.ClientRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Optional;
 
@@ -15,18 +18,14 @@ import java.util.Optional;
 
 @Controller
 public class ArtistController {
+
     private ArtistRepository artistRepository;
+    private Logger logger = LoggerFactory.getLogger(ArtistController.class);
 
     public ArtistController(ArtistRepository artistRepository) {
         this.artistRepository = artistRepository;
     }
 
-    @GetMapping("/artistlist")
-    public String artistList(Model model) {
-        Iterable<Artist> artists = artistRepository.findAll();
-        model.addAttribute("artists", artists);
-        return "artistlist";
-    }
 
     @GetMapping({"/artistdetails/{id}", "/artistdetails"})
         public String artistDetails (Model model, @PathVariable(required = false) Integer id) {
@@ -41,6 +40,18 @@ public class ArtistController {
 
         }
         return "artistdetails";
+    }
+
+
+    @GetMapping("/artistlist")
+    public String artistList(Model model,  @RequestParam(required = false) String search) {
+        logger.info("artistlist: search={}", search);
+
+        final Iterable<Artist> artists = artistRepository.findByFilter(search);
+        model.addAttribute("artists", artists);
+        long artistCount = artistRepository.count();
+        model.addAttribute("artistCount", artistCount);
+        return "artistlist";
     }
 
 }
--- src/main/java/be/thomasmore/party/repositories/ArtistRepository.java
+++ src/main/java/be/thomasmore/party/repositories/ArtistRepository.java
@@ -1,6 +1,21 @@
 package be.thomasmore.party.repositories;
 import be.thomasmore.party.model.Artist;
+import be.thomasmore.party.model.Venue;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
-public interface ArtistRepository extends CrudRepository<Artist,Integer> {
+import java.util.List;
+
+//public interface ArtistRepository extends CrudRepository<Artist, Integer> {
+//
+//    @Query("SELECT a FROM Artist a WHERE :search IS NULL OR a.artistName LIKE %:search%")
+//    List<Artist> findByFilter(@Param("search") String search);
+//}
+public interface ArtistRepository extends CrudRepository<Artist, Integer> {
+
+    @Query("SELECT a FROM Artist a " +
+            "WHERE :search IS NULL OR LOWER(a.artistName) LIKE LOWER(CONCAT('%', :search, '%'))")
+    List<Artist> findByFilter(@Param("search") String search);
 }
+
